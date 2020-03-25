79e18bc00d66e6d0325dc0add46f533acc25183f
alirn76 <alirn76@yahoo.com>
2020-03-25 22:14:36 +0430

    media root ADD shod

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class Item(models.Model):
     id          = models.AutoField(db_column='ID', primary_key=True)
-    imageUrl    = models.ImageField(db_column="imageUrl", max_length=256, upload_to='images/')
+    imageUrl    = models.ImageField(db_column="imageUrl", upload_to='images/', max_length=256)
     name        = models.CharField(db_column='Name', max_length=128, null=True)
     category    = models.CharField(db_column='Category', max_length=128, null=True)
     description = models.CharField(db_column='Description', max_length=256,  blank=True, null=True)
@@ -14,7 +14,6 @@ class Item(models.Model):
     price       = models.IntegerField(db_column='Price', null=True)
 
     class Meta:
-        managed = False
         db_table = 'Item'
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
