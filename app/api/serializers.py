4272dcd8b39ac5037bb73812a4ed48d4e55036c0
alirn76 <alirn76@yahoo.com>
2020-03-27 21:17:28 +0430

    fixed a bug

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -4,4 +4,4 @@ from app.models import Item
 class ItemSerializers(serializers.ModelSerializer):
     class Meta:
         model   = Item
-        fields  = ['name', 'category', 'description', 'number', 'price']
+        fields  = ['image_url', 'name', 'category', 'description', 'number', 'price']
