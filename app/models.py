c0b8ba04cf01203de18addf98ad75f324c392f63
alirn76 <alirn76@yahoo.com>
2020-03-29 01:01:08 +0430

    __str__ baraye Item ADD shod

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class Item(models.Model):
     id          = models.AutoField(db_column='ID', primary_key=True)
-    image_url    = models.ImageField(db_column="Image_Url", upload_to='images/', max_length=256)
+    image_url   = models.ImageField(db_column="Image_Url", upload_to='images/', max_length=256)
     name        = models.CharField(db_column='Name', max_length=128)
     category    = models.CharField(db_column='Category', max_length=128)
     description = models.CharField(db_column='Description', max_length=256,  blank=True, null=True)
@@ -15,6 +15,9 @@ class Item(models.Model):
 
     class Meta:
         db_table = 'Item'
+
+    def __str__(self):
+        return self.name
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
